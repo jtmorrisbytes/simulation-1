80dde763adcfde649ddc2f0f450ba264b2e451f4
= <=>
2020-04-03 15:15:49 -0500

    starting on dashboard

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Dashboard.css";
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = [];
+  }
+  render() {
+    return <div className="Dashboard">Dashboard</div>;
+  }
+}
+export default Dashboard;
